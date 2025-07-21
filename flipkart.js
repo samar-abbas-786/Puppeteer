792ed0860884dd05c1969c97f2c6f286c276ef9c
Samar Abbas <samarabbas172003@gmail.com>
2025-07-21 07:26:30 +0530

    flipcart scrapesssh

--- flipkart.js
+++ flipkart.js
@@ -1,49 +1,65 @@
 import puppeteer from "puppeteer";
 import fs from "fs/promises";
+import { log } from "console";
 
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto("https://www.flipkart.com/search?q=shirts");
+  await page.goto("https://www.flipkart.com/search?q=pants", {
+    waitUntil: "networkidle2",
+  });
 
   const results = [];
-  let c = 13000;
+  let c = 1000;
+
   try {
     while (c > 0) {
-      await page.waitForSelector(".DOjaWF .gdgoEp ");
-      const list = await page.$$(".DOjaWF .gdgoEp .cPHDOP ._75nlfW");
-      for (const product of list) {
+      await page.waitForSelector(".DOjaWF .gdgoEp");
+
+      const products = await page.$$(".DOjaWF .gdgoEp .cPHDOP ._75nlfW");
+
+      for (const product of products) {
         if (c <= 0) break;
 
-        const title = await page.evaluate((el) => {
+        const data = await page.evaluate((el) => {
           const titleEl = el.querySelector(".hCKiGj > .syl9yP");
           const imgEl = el.querySelector("img._53J4C-");
           const priceEl = el.querySelector("div.Nx9bqj");
 
-          const titleText = titleEl ? titleEl.textContent.trim() : "N/A";
-          const imageSrc = imgEl ? imgEl.src : "N/A";
-          const price = priceEl ? priceEl.textContent.trim() : "N/A";
-
-          return { titleText, imageSrc, price };
+          return {
+            title: titleEl?.textContent.trim() || "N/A",
+            imageSrc: imgEl?.src || "N/A",
+            price: priceEl?.textContent.trim() || "N/A",
+          };
         }, product);
+
+        results.push(data);
+        console.log(data);
         c--;
-        results.push(title);
-        console.log(title);
       }
-      const buttons = await page.$$("._9QVEpD");
       await page.waitForSelector("._9QVEpD", { visible: true });
+      const buttons = await page.$$("._9QVEpD");
 
       for (const btn of buttons) {
         const text = await page.evaluate((el) => el.textContent.trim(), btn);
 
         if (text === "Next") {
+          await page.waitForNetworkIdle({ idleTime: 500 });
           await btn.click();
-          break;
+          //   page.waitForNavigation({ waitUntil: "networkidle2" });
         }
       }
     }
+
     await fs.writeFile("data.json", JSON.stringify(results, null, 2), "utf8");
+    log("length", results.length);
+    console.log("✅ Data successfully saved to data.json");
   } catch (error) {
-    // await fs.writeFile("data.json", JSON.stringify(results, null, 2), "utf8");
+    console.error("❌ Error occurred:", error.message);
+    await fs.writeFile("data.json", JSON.stringify(results, null, 2), "utf8");
+    log("length", results.length);
+  } finally {
+    await browser.close();
+    log("length", results.length);
   }
 })();
